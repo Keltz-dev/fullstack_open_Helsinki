18699941c4eb82d2a8aec5e881551210a21435f1
Karl von der Eltz <karlvondereltz@gmail.com>
2024-03-05 12:41:06 +0100

    render Statistics conditionally

--- exercise_1.6/.eslintrc.cjs
+++ exercise_1.6/.eslintrc.cjs
@@ -17,6 +17,6 @@ module.exports = {
       "warn",
       { allowConstantExport: true },
     ],
-    "reacts/prop-types": 0,
+    "react/prop-types": 0,
   },
 };
--- exercise_1.6/src/components/Statistics.jsx
+++ exercise_1.6/src/components/Statistics.jsx
@@ -1,21 +1,26 @@
 const Statistics = ({ good, bad, neutral }) => {
   const all = good + bad + neutral;
-  const allAsDivider = () => {
-    return all === 0 ? 1 : all;
-  };
 
-  return (
-    <>
-      <h1>Statistics</h1>
-      <div>good: {good}</div>
-      <div>neutral: {neutral}</div>
-      <div>bad: {bad}</div>
-      <div>all: {all}</div>
-      <div>average: {(good - bad) / allAsDivider()}</div>{" "}
-      {/* On a scale of -1 to 1 */}
-      <div>positive: {good / allAsDivider()}</div>
-    </>
-  );
+  if (all === 0) {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <div>No feedback gathered</div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1>Statistics</h1>
+        <div>good: {good}</div>
+        <div>neutral: {neutral}</div>
+        <div>bad: {bad}</div>
+        <div>all: {all}</div>
+        <div>average: {(good - bad) / all}</div> {/* On a scale of -1 to 1 */}
+        <div>positive: {good / all}</div>
+      </>
+    );
+  }
 };
 
 export default Statistics;
